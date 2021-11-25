4ddef1cee1164f17f7d5f1c4804e566cf50d0c80
Oikmo <78755068+oikmo@users.noreply.github.com>
2021-11-25 18:33:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+
+# Project Title
+
+A Story Game Made With Python 
+
+A Game Which Can Be Ran In A PC Console
+
+
+## Authors
+
+- [@NoTrash3605 {Github}](https://github.com/masteroof)
+- @Gong-yoo#2889 {Discord}
+- [@Oikmo](https://github.com/oikmo)
+- @Oikmo#9889 {Discord}
+- [@LycnK](https://LinkGoesHere.Com)
+
+## Device Support
+
+PC
+
+Mac
+
+Linux
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ main.py
@@ -0,0 +1,69 @@
+#imports
+import os
+import time
+import colorama as cr
+import math
+import sys
+
+
+cr.init(autoreset=True)
+
+
+#clears the console every now and then
+def clearConsole():
+    command = 'clear'
+    os.system(command)
+
+
+#the actual code :noooooo:
+
+print(f"{cr.Fore.GREEN}this part is crucial to do") 
+time.sleep(3)
+clearConsole()
+
+play(song)
+print("Loading main.py {⬜         } 20%") #Loading
+time.sleep(2)
+clearConsole()
+print("Loading main.py {⬜⬜       } 40%") #Loading
+time.sleep(2)
+clearConsole()
+print("Loading main.py {⬜⬜⬜     } 60%") #Loading
+time.sleep(2)
+clearConsole()
+print("Loading main.py {⬜⬜⬜⬜   } 80%") #Loading
+time.sleep(2)
+clearConsole()
+print("Loading main.py {⬜⬜⬜⬜⬜} 100%") #Loading
+time.sleep(2)
+clearConsole()
+
+user = input(f"{cr.Fore.GREEN}USER? : ")
+time.sleep(1)
+clearConsole()
+
+print(f"{cr.Fore.GREEN}Mysterious:Wake up " + user + ".")
+time.sleep(0.5)
+print(f"{cr.Fore.GREEN}Mysterious:You don't have enough time to WASTE.")
+
+time.sleep(2)
+print(f"{cr.Fore.LIGHTBLUE_EX}Respond With: | What | Where Am I |")
+time.sleep(1)
+
+user_input1 = input(f"{cr.Fore.LIGHTRED_EX} " + user + " : ")
+
+time.sleep(2)
+
+if user_input1=="What":
+    print(f"{cr.Fore.GREEN}Mysterious:Wait Did You Lose Your Memory?")
+
+if user_input1=="Where Am I":
+    print(f"{cr.Fore.GREEN}Mysterious:Wait Did You Lose Your Memory?")
+
+if user_input1.lower() == "what" or user_input1.lower() == 'where am i':
+    print(f"{cr.Fore.GREEN}Mysterious:Wait Did You Lose Your Memory?")
+
+else:
+    print(f"{cr.Fore.RED}Game.Console: '" + user_input1 +"' Considered to be a input Error. Please restart game. :(")
+    sys.exit("")
+time.sleep(2)
